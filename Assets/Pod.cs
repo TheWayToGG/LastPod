e7824004a12c2c7f76a49bf1d7c0ea4e81ad758a
TheWay <k5x3s1@gmail.com>
2020-04-20 22:00:05 +0200

    Added basic input handling

---
+++ Assets/Pod.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pod : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Accelerating!");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Rotating left!");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Rotating right!");
+        }
+    }
+}
