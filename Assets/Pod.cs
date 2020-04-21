b9db04330bf8f27cdbf6ac59f65236d485614f63
TheWay <k5x3s1@gmail.com>
2020-04-21 18:33:28 +0200

    Added basic movement of the pod

--- Assets/Pod.cs
+++ Assets/Pod.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Pod : MonoBehaviour
 {
+
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,14 +24,19 @@ public class Pod : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             print("Accelerating!");
+            rigidbody.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            //rigidbody.AddRelativeTorque(0, 0, 0.1f);
+            transform.Rotate(Vector3.forward);
             print("Rotating left!");
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            //rigidbody.AddRelativeTorque(0, 0, -0.1f);
+            transform.Rotate(Vector3.back);
             print("Rotating right!");
         }
     }
