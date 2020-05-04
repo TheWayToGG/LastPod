e4729e46600053177b1044130dde3c6fdb9ffcb4
TheWay <k5x3s1@gmail.com>
2020-05-04 14:30:04 +0200

    Added oscillator script for a single obstacle and created level 0

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movementVector;
+
+    // todo remove from inspector
+    [Range(0, 1)]
+    [SerializeField]
+    float movementFactor; // 0 for full stop, 1 for max speed
+
+    Vector3 startingPosition;
+    Vector3 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offSet = movementVector * movementFactor;
+        transform.position = startingPosition + offSet;
+    }
+}
